79322c7343e81b82a518d08117021ff0bef742f1
gg_hatano <adad0405@gmail.com>
2014-12-13 12:40:47 +0900

    選挙区 + 名前空白はダメそう

---
+++ search.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+dir=$(dirname $0)
+
+
+[ -e "$dir/kouho.hit.list" ]  && rm $dir/kouho.hit.list
+
+cat $dir/kouho.list | while read line 
+do
+  echo $line
+  searchWord=$(echo $line | cut -d, -f1,2 | sed 's/[, ]/+/g')
+  echo $searchWord
+  url=$(echo "http://www.bing.com/search?q="$searchWord"&go=送信&qs=n&form=QBLH&pq="$searchWord"&sc=0-0&sp=-1&sk=")
+
+  wget $url -O $dir/tmp/res.txt
+
+  hit=$(cat $dir/tmp/res.txt | nkf -w8 | 
+  grep "検索結果" | 
+  gsed 's/検索結果/\n検索結果\n/'| head -n 1 | 
+  awk -F "(<|>)" '{print $NF}' | 
+  sed 's/[^0-9]//g')
+
+  echo $line$hit >> $dir/kouho.hit.list
+  sleep 1
+done
